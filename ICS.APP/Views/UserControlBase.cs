0956353eea6a89f95d4979751276d6f16cadaadc
Martin Dvořák <32228289+MartinDvorak@users.noreply.github.com>
2019-10-19 07:49:20 +0200

    migration

---
+++ ICS.APP/Views/UserControlBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using TeamsManager.APP.ViewModels.BaseViewModel;
+
+namespace TeamsManager.APP.Views
+{
+    public abstract class UserControlBase : UserControl
+    {
+        protected UserControlBase() => Loaded += OnLoaded;
+
+        private void OnLoaded(Object sender, RoutedEventArgs e)
+        {
+            if (DataContext is IViewModel viewModel)
+            {
+                viewModel.Load();
+            }
+        }
+    }
+}
